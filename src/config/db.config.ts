992b062d5d53a57d21fa1f8a4a4f3d80335c5110
Kevin Dave <kevindave1308@gmail.com>
2026-04-17 11:53:30 +0530

    authjwt-done-01

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -6,7 +6,7 @@ console.log(process.env.DB);
 const db : Pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER1,
-    database: process.env.DB,
+    database: process.env.DB,xcvxcvxcvxcv
     password:process.env.PASSWORD,
     port: parseInt(process.env.DBPORT!),
     waitForConnections: true,
