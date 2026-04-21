0d702f239e92813f8e346e1a14c89417227c4a60
Kevin Dave <kevindave1308@gmail.com>
2026-04-21 18:41:53 +0530

    fixes

--- src/middlewares/captcha.ts
+++ src/middlewares/captcha.ts
@@ -4,25 +4,22 @@ import { Request, Response } from 'express';
 
 declare module "express-session" {
   interface SessionData {
-    captcha?: string; // Add your custom properties here
+    captcha?: string; 
   }
 }
 
 export const authCaptcha = async(req:Request,res:Response)=>{
-    // Only generate new captcha if one doesn't exist
-    if (!(req.session as any).captcha) {
-      const captcha = svgCaptcha.create({
-        size: 4, // length of random string
-        noise: 2, // number of noise lines
-        color: true,
-        background: '#f0f0f0'
-      });
-      (req.session as any).captcha = captcha.text;
-      (req.session as any).captchaImage = captcha.data;
-    }
+    // Always generate a new captcha
+    const captcha = svgCaptcha.create({
+      size: 4, // length of random string
+      noise: 2, // number of noise lines
+      color: true,
+      background: '#f0f0f0'
+    });
+    (req.session as any).captcha = captcha.text;
 
   // Send the SVG image data to the React frontend
   res.type('svg');
-  res.status(200).send((req.session as any).captchaImage);
+  res.status(200).send(captcha.data);
   
 }
